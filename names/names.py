4d3e93f837faadf5c5433d9dd7b59dc98df6e16a
TheHaunted <kevin_lopez@live.com>
2020-01-31 12:44:54 -0600

    MVP

--- names/names.py
+++ names/names.py
@@ -1,3 +1,55 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        
+        if self.value < target and self.right:
+            if self.right:
+                return self.right.contains(target)
+        elif self.value >= target and self.left:
+            if self.left:
+                return self.left.contains(target)
+        else:
+            return False
+
+        
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 import time
 
 start_time = time.time()
@@ -11,10 +63,15 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+bst = BinarySearchTree(names_1[0])
+
+for name in names_1[1:]:
+    bst.insert(name)
+
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
