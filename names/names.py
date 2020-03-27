e895717a8e874b32b4ba7fa8053417467693b84f
TheHaunted <kevin_lopez@live.com>
2020-03-27 07:36:28 -0500

    .

--- names/names.py
+++ names/names.py
@@ -1,55 +1,4 @@
-class BinarySearchTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
 
-    # Insert the given value into the tree
-    def insert(self, value):
-        if value < self.value:
-            if self.left is None:
-                self.left = BinarySearchTree(value)
-            else:
-                self.left.insert(value)
-
-        else:
-            if self.right is None:
-                self.right = BinarySearchTree(value)
-            else:
-                self.right.insert(value)
-
-    # Return True if the tree contains the value
-    # False if it does not
-    def contains(self, target):
-        if self.value == target:
-            return True
-        
-        if self.value < target and self.right:
-            if self.right:
-                return self.right.contains(target)
-        elif self.value >= target and self.left:
-            if self.left:
-                return self.left.contains(target)
-        else:
-            return False
-
-        
-
-    # Return the maximum value found in the tree
-    def get_max(self):
-        if self.right:
-            return self.right.get_max()
-        else:
-            return self.value
-
-    # Call the function `cb` on the value of each node
-    # You may use a recursive or iterative approach
-    def for_each(self, cb):
-        cb(self.value)
-        if self.left:
-            self.left.for_each(cb)
-        if self.right:
-            self.right.for_each(cb)
 import time
 
 start_time = time.time()
@@ -62,16 +11,13 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
+duplicates = []  # Return the list of duplicates in this data structure
 
-bst = BinarySearchTree(names_1[0])
-
-for name in names_1[1:]:
-    bst.insert(name)
-
-for name in names_2:
-    if bst.contains(name):
-        duplicates.append(name)
+# Replace the nested for loops below with your improvements
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -79,5 +25,5 @@ print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish with no restrictions on techniques or data
-# structures?
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
